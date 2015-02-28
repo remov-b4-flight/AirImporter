dcdf60984630c1b32216371fd9d70b2c289d1fb9
jenoki <jenoki48@gmail.com>
2015-03-01 03:44:06 +0900

    some clean up

--- AirImporter.lua
+++ AirImporter.lua
@@ -16,46 +16,49 @@ local AiDate = import 'LrDate'
 local AiDialogs = import 'LrDialogs'
 local AiFileUtils = import 'LrFileUtils'
 
-local FAhost='flashair'
+-- Constants --
+local FLASHAIR_HOST='flashair'
 local DCIM = '/DCIM'
-local Command ='command.cgi'
-local URLhost = 'http://' .. FAhost
-local URLhead = URLhost .. '/' .. Command .. '?'
-local LsCmd = 'op=100'
+local CMDCGI ='command.cgi'
+local URLHOST = 'http://' .. FLASHAIR_HOST
+local LSCMD = 'op=100'
 local PictureFolder = '/Users/jenoki/Pictures/TEST'
+local URLCGI = URLHOST .. '/' .. CMDCGI .. '?'
+local URLListCmd = URLCGI .. LSCMD.. '&DIR='
 
+--
 local filepath={}
 if prefs.isLog then
 	AiLogger:enable('logfile')
 end
 AiLogger:info(PluginTitle ..' START')
 if prefs.AutoSplit then 
-	AiLogger:info('AutoSplit is on') 
+	AiLogger:info('AutoSplit is on.') 
 	AiLogger:info('Interval = ' .. prefs.Interval .."min.")
 end
 
 local CurrentCatalog = AiApplication:activeCatalog()
 
---Get Lists of folders (top of hiearchy)
-local URLListCmd=URLhead .. LsCmd.. '&DIR='
+-- Main part --
 AiTasks.startAsyncTask(
 	function ()
 		local ProgressBar = AiProgress({
-			title = 'Access to: ' .. FAhost
+			title = 'Access to: ' .. FLASHAIR_HOST
 		})
 		--confirm FlashAir Host is online
-		local result,headers = AiHttp.get(URLhost)
+		local result,headers = AiHttp.get(URLHOST)
 		if headers.status ~= 200 then
-			local errmsg = 'Host ' .. FAhost .. ' is not online.'
+			local errmsg = 'Host ' .. FLASHAIR_HOST .. ' is not online.'
 			AiLogger:error(errmsg)
 			AiDialogs.showError(errmsg)
+			ProgressBar:cancel()
 			return
 		end
 		ProgressBar:setPortionComplete(10,100)
-		--Recursive search files
+		--Recursive search files from DCIM
 		AiLogger:debug('Begin GetBranch on ' .. DCIM)
 		GetBranch(DCIM)
-		AiLogger:debug('All GetBranch end')
+		AiLogger:debug('All GetBranch end.')
 		ProgressBar:setPortionComplete(20,100)
 		table.sort(filepath)
 		--AutoSplit preprocess
@@ -80,8 +83,8 @@ AiTasks.startAsyncTask(
 			lastdatetime = datetime
 		end
 		ProgressBar:setPortionComplete(30,100)
-		destdate[destindex]=AiDate.timeToIsoDate(datetime) --procees last item
-		-- de-duplication
+		destdate[destindex]=AiDate.timeToIsoDate(datetime) --process last item
+		-- De-duplication
 		local fidx = 1
 		local destfolder={}
 		for i,v in ipairs(destdate) do
@@ -100,17 +103,17 @@ AiTasks.startAsyncTask(
 			end
 		end
 		ProgressBar:setPortionComplete(40,100)
-		-- make folders
+		-- Make target folders
 		for i,v in ipairs(destfolder) do
 			AiLogger:debug(i..' destfolder=' .. v)
 			local pathToMake = PictureFolder .. '/' .. v
 			AiFileUtils.createDirectory(pathToMake)
 		end
 		ProgressBar:setPortionComplete(50,100)
-		-- make config file for curl
+		-- Make config file for curl
 		local NL='\n'
 		local curloptfile=os.tmpname()
-		AiLogger:debug('temp curl config=' .. curloptfile)
+		AiLogger:debug('temp. curl config=' .. curloptfile)
 		local copt_fh=io.open(curloptfile,'w')
 		local countfiles=#filepath
 		for i,v in pairs(filepath) do
@@ -118,7 +121,7 @@ AiTasks.startAsyncTask(
 			local dummy,upath = string.match(filepath[i],'(.-):(.+)')
 			local dm1,dm2,tailfilename=string.match(upath,'/(.-)/(.-)/(.+)')
 			local outfile = PictureFolder .. '/' .. df .. '/' .. tailfilename
-			local cmdopt ='output="' .. outfile ..'"' .. NL ..'url="'.. URLhost .. upath .. '" '.. NL
+			local cmdopt ='output="' .. outfile ..'"' .. NL ..'url="'.. URLHOST .. upath .. '" '.. NL
 			--AiLogger:debug(cmdopt)
 			copt_fh:write(cmdopt)
 		end
@@ -146,10 +149,13 @@ AiTasks.startAsyncTask(
 		ProgressBar:done()
 	end
 )
---
+--[[
+Descend & capture DCIM folders structure.
+]]
 function GetBranch(base)
 	AiLogger:debug('GetBranch() entry')
 	AiLogger:debug('base=' .. base)
+
 	local url = URLListCmd .. base
 	AiLogger:debug('access url=' .. url)
 	local result,headers = AiHttp.get(url)
@@ -187,7 +193,7 @@ end
 	Utility functions for string processing.
 	Copyright (C) 2007 Moritz Post <mail@moritzpost.de>
 	Released under the GNU GPL.
-	-----------------------------------------------------------------------------]]--
+	-----------------------------------------------------------------------------]]
 function string.split( _str, _sep ) 
 	local t = {}
 	local sep = _sep
